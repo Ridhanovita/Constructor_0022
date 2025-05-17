747d8b1e2986c9db43cd0abab9cb213e099ba117
Ridhanovita <novitaridha796@gmail.com>
2025-05-17 17:12:08 +0700

    deklarasi class peminjam

--- LatihanPertemuan9B.cpp
+++ LatihanPertemuan9B.cpp
@@ -13,7 +13,18 @@ private:
 public:
     Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
 
-    friend class Petugas; // friend class
-    friend void lihatStatistik(const Buku *b); // friend function
+    friend class Petugas;
+    friend void lihatStatistik(const Buku *b); 
 };
 
+class Peminjam {
+private:
+    string nama;
+    int totalPinjaman;
+public:
+    Peminjam();
+    Peminjam(string);
+
+    friend class Petugas;
+    friend void lihatStatistik(*Peminjam);
+};
