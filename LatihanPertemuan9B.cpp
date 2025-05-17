8285c5449a2270220f44bd6221fea1fbd1359096
Ridhanovita <novitaridha796@gmail.com>
2025-05-17 17:10:26 +0700

    deklarasi class buku

--- LatihanPertemuan9B.cpp
+++ LatihanPertemuan9B.cpp
@@ -3,3 +3,17 @@
 using namespace std;
 
 class Peminjam;
+
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    friend class Petugas; // friend class
+    friend void lihatStatistik(const Buku *b); // friend function
+};
+
